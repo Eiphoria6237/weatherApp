0cc894f46f1b62ae2a280a7c04190bf6df5419a1
Eiphoria <liuerbsb@gmal.com>
2021-04-05 01:01:20 +0900

    API- search weather by city name

--- src/index.js
+++ src/index.js
@@ -26,9 +26,26 @@ showMin.innerHTML = minute
 
 
 //search box
+function showTemp(response) {
+    let tempNow = document.getElementById("tempNowNum")
+    let tempNowValue = Math.round(response.data.main.temp)
+    document.getElementById("search-text-input").placeholder = response.data.name
+    tempNow.innerHTML = tempNowValue
+}
+
 function handleSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-text-input")
+    let city = document.getElementById("search-text-input")
+    let cityName = city.value
+    let apiKey = "92e1bb4d476d213f7b8e7becfa583f21"
+    let apiPrefix = "http://api.openweathermap.org/data/2.5/weather?"
+    let apiURL = `${apiPrefix}q=${cityName}&appid=${apiKey}&units=metric`
+
+
+    axios.get(apiURL).then(showTemp)
+
+    //show city name
     document.getElementById("search-text-input").placeholder = searchInput.value
     document.getElementById("search-text-input").value = ""
     
@@ -38,17 +55,65 @@ form.addEventListener("submit", handleSubmit)
 
 
 //CF button
+function changeCF(read) {
+    let temp = Math.round(read.data.main.temp)
+    return temp
+}
+
 function cfFunc() {
+    
+    let city = document.getElementById("search-text-input")
+    let cityName = city.value
+    let apiKey = "92e1bb4d476d213f7b8e7becfa583f21"
+    let apiPrefix = "http://api.openweathermap.org/data/2.5/weather?"
+    let apiURL = `${apiPrefix}q=${cityName}&appid=${apiKey}&units=metric`
+    let apiURLF = `${apiPrefix}q=${cityName}&appid=${apiKey}&units=imperial`
+
+    let C = axios.get(apiURL).then(changeCF)
+    let F = axios.get(apiURLF).then(changeCF)
+
+
     let x = document.getElementById("tempNowNum");
-    if (Number(x.innerHTML) === 21) {
-        x.innerHTML = Math.round((21 * 9 / 5) + 32);
+    if (Number(x.innerHTML) === C) {
+        x.innerHTML = F;
     } else {
-        x.innerHTML = 21
+        x.innerHTML = C
     }
     
 }
 
 let cfButton = document.getElementById("CF")
 cfButton.addEventListener("click", cfFunc)
+
+
+//current location search engine
+
+function handleGeo (position) {
+    let lat = position.coords.latitude
+    let lon = positions.coords.longitude
+
+
+    let apiKey = "92e1bb4d476d213f7b8e7becfa583f21"
+    let apiPrefix = "http://api.openweathermap.org/data/2.5/weather?"
+    let apiURL = `${apiPrefix}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+
+    axios.get(apiURL).then(showTemp)
+}
+
+function geoInfo() {
+    navigator.geolocation.getCurrentPosition(handleGeo)
+}    
+
+
+/*
+let CLbutton = document.getElementsByClassName("geobutton")
+CLbutton.addEventListener("click", geoInfo)
+*/
+
+
+
+
+
+
 
 
